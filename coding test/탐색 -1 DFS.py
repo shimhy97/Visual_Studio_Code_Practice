6c6b51dcfed3d143cb9b5b47a4d4bc677db0f447
shimhy97 <shimhy97@korea.ac.kr>
2022-01-11 00:57:37 +0900

    210111_DFS

---
+++ coding test/탐색 -1 DFS.py
@@ -0,0 +1,23 @@
+# DFS
+
+def DFS(graph,v,visited):
+    visited[v]=True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            DFS(graph,i,visited)    
+
+visited = [False]*9
+graph = [
+[],
+[2,3,8],
+[1,7],
+[1,4,5],
+[3,5],
+[3,4],
+[7],
+[2,6,8],
+[1,7]
+]
+
+DFS(graph,1,visited)
